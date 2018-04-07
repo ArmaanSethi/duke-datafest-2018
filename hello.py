e9c10cb1ba37b3590be252c1e96a0199ca1c61b1
justinmmott <justinmmott@gmail.com>
2018-04-07 18:03:11 -0400

    first graph

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 import pickle
+import numpy as numpy
+import matplotlib.pyplot as plt
+import re
+import csv
 
 dates = pickle.load( open( "dates.p", "rb"))
 duration = pickle.load( open( "duration.p", "rb"))
 dbc = pickle.load( open("days_between_clicks.p", "rb"))
-
+count = 0
+for d in duration.keys():
+	count = count + 1
+	plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), int(re.search(r'\d+', str(duration[d])).group()))
+	if count ==1000:
+		print("reach 1k")
+		break
+plt.show()
