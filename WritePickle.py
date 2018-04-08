63b9b0d40ed9d4e9134acb07a393f974f648583a
justinmmott <justinmmott@gmail.com>
2018-04-07 23:44:19 -0400

    august's work

--- WritePickle.py
+++ WritePickle.py
@@ -2,25 +2,65 @@ import csv
 import pickle
 from tqdm import tqdm
 
-experienceRequired = {} 
+# experienceRequired = {} 
+# salaryArray = {}
+# companyRating = {}
+wordCount = {}
 with open('datafest2018.csv', encoding = "utf8") as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
-    for idx,row in tqdm(enumerate(csvReader)):
-        if(idx < 1):
-            print(row)
-        else:
-            country = row[3]
-            jobID = row[2]
-            companyID = row[1]
-            experience = row[13]
-            if experience == "":
-            	experience = 0
-            #print(experience)
-            if(country == 'US'):
-            	if jobID in experienceRequired:
-            		if experienceRequired[jobID] == 0:
-            			experienceRequired[jobID] = experience
-            	else: 
-            		experienceRequired[jobID] = experience
+	csvReader = csv.reader(csvDataFile)
+	for idx,row in tqdm(enumerate(csvReader)):
+		if(idx < 1):
+			print(row)
+		else:
+			country = row[3]
+			jobID = row[2]
+			companyID = row[1]
+			# experience = row[13]
+			# if experience == "":
+			# 	experience = 0
+			# #print(experience)
+			# if(country == 'US'):
+			# 	if jobID in experienceRequired:
+			# 		if experienceRequired[jobID] == 0:
+			# 			experienceRequired[jobID] = experience
+			# 	else: 
+			# 		experienceRequired[jobID] = experience
 
-pickle.dump(experienceRequired, open("experience.p", "wb"))
+			# salary = row[14]
+			# if (country == 'US'):
+			# 	if jobID in salaryArray:
+			# 		if salary > salaryArray[jobID]:
+			# 			salaryArray[jobID] = salary
+			# 	else:
+			# 		salaryArray[jobID] = salary
+
+			# cRating = row[6]
+			# if cRating == "":
+			# 	cRating = 0;
+			# if (country == 'US'):
+			# 	if float(cRating) > -0.1:
+			# 		if jobID in companyRating:
+			# 			if cRating > companyRating[jobID]:
+			# 				companyRating[jobID] = cRating
+			# 		else:
+			# 			companyRating[jobID] = cRating
+
+			words = row[12]
+			if (country == 'US'):
+				if jobID in wordCount:
+					if words > wordCount[jobID]:
+						wordCount[jobID] = words
+				else:
+					wordCount[jobID] = words
+
+
+
+# pickle.dump(experienceRequired, open("experience.p", "wb"))
+# pickle.dump(salaryArray, open("salary.p", "wb"))
+# pickle.dump(companyRating, open("companyRating.p", "wb"))
+pickle.dump(wordCount, open("wordCount.p", "wb"))
+
+
+
+
+
--- hello.py
+++ hello.py
@@ -3,18 +3,29 @@ import numpy as numpy
 import matplotlib.pyplot as plt
 import re
 import csv
+import datetime
 
 dates = pickle.load( open( "dates.p", "rb"))
 duration = pickle.load( open( "duration.p", "rb"))
 dbc = pickle.load( open("days_between_clicks.p", "rb"))
 experience = pickle.load( open("experience.p", "rb"))
+# salary = pickle.load( open("salary.p", "rb"))
+# companyRating = pickle.load( open("companyRating.p", "rb"))
+# wordCount = pickle.load( open("wordCount.p", "rb"))
 count = 0
-
-
+# print(len(duration))
+# print(len(companyRating))
 for d in duration.keys():
-	count = count + 1
-	plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), experience[d])
-	if count ==1000:
-		print("reach 1k")
-		break
+# for d in companyRating.keys():
+	if int(re.search(r'\d+', str(duration[d])).group()) < 99:
+		count = count + 1
+		plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), experience[d])
+		# plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), salary[d])
+		# plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), companyRating[d])
+		# plt.scatter(int(re.search(r'\d+', str(duration[d])).group()), wordCount[d])
+		if count == 10000:
+			print("reach 1k")
+			break
+		if count%1000 == 0:
+			print("reach " + str(count/1000) + "k")
 plt.show()
